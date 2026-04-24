316f384c9be9702ee1a60dd517d3f685aad95433
cs26-69 <benjamin.duscio@student.unisg.ch>
2026-04-24 21:00:03 +0200

    Erstes Slider Setup

--- app.py
+++ app.py
@@ -1,4 +1,45 @@
-import streamlit as streamlit
-st.write ("Hooray, we connected everything")
+import streamlit as st
+from datetime import date
 
-st.write("Hello")
+st.title("FitMyTrip")
+
+st.header("Finde dein Traumreiseziel in Europa!")
+
+st.markdown("Bist auch Du überwältigt von der Auswahl verschiedener Reiseziele? " \
+        "Kein Problem! Unsere App liefert dir ein **personalisiertes Reiseziel** " \
+        "basierend auf verschiedenen **Matchmaking Kriterien**.")
+
+st.write("Folgende Kriterien stehen zur Auswahl:")
+
+
+st.write("Temperatur")
+temp = st.slider("Bitte gib deine gewünschte Temperatur am Reiseziel ein: ", -20, 40, 0)
+
+
+st.write("Sicherheit")
+safety = st.slider("Bitte gib deine gewünschte Sicherheit (1) am Reiseziel ein:", 1, 10, 1)
+st.write("(1) Gemessen anhand Global Safety Indey gemäss XYZ")
+
+st.write("Reisedauer")
+dist = st.slider("Bitte gib deine gewünschte Flugzeit von Zürich in Stunden ein: ",0, 5, 0)
+
+
+st.write("Budget")
+budget = st.number_input("Bitte gib dein gewünschtes Budget für die Reise ein:", min_value = 0, step = 1)
+
+
+st.write("Reisezeitraum")
+trip_start = st.date_input("Bitte gib dein gewünschten Startzeitpunkt ein:")
+trip_end = st.date_input("Bitte gib dein gewünschten Endzeitpunkt ein:")
+
+trip_duration = trip_end - trip_start
+
+trip = {
+    "Temperatur": temp, "Sicherheit": safety, "Entfernung": dist, "Budget": budget, "Reisestart": trip_start, "Reiseende": trip_end, "Reisedauer": trip_duration
+}
+
+st.subheader("Zusammenfassung")
+st.write("Du hast folgende Inputfaktoren gewählt: ")
+
+for key, value in trip.items():
+    st.markdown(f"**{key}:** {value}")
