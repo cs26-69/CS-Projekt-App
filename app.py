7c49415c6e74a212c5e4bd2c1da4ec9a6b6ce769
cs26-69 <269434708+cs26-69@users.noreply.github.com>
2026-04-24 23:07:21 +0200

    Anpassung Input Slider

--- app.py
+++ app.py
@@ -9,16 +9,22 @@ st.markdown("Bist auch Du überwältigt von der Auswahl verschiedener Reiseziele
         "Kein Problem! Unsere App liefert dir ein **personalisiertes Reiseziel** " \
         "basierend auf verschiedenen **Matchmaking Kriterien**.")
 
+st.subheader("Kriterien Input")
+
 st.write("Folgende Kriterien stehen zur Auswahl:")
+#Die Inputs werden in den Variablen category, temperature, safety, flighttime, bduget, trip_duration abgespeichert
+
+st.write("Art des Reiseziels")
+category = st.selectbox("Bitte wähle deine gewünschte Kategorie von Reiseziel:", ["Berge", "Meer", "Stadt", "Natur"], index = None, placeholder = "Bitte wählen")
 
 st.write("Temperatur")
 temperature = st.slider("Bitte gib deine gewünschte Temperatur am Reiseziel ein: ", -30, 40, 0)
 
 st.write("Sicherheit")
-safety = st.slider("Bitte gib deine gewünschte Sicherheit am Reiseziel ein:", 1, 10, 1)
+safety = st.slider("Bitte gib deine gewünschtes Sicherheitsniveau am Reiseziel ein:", 1, 10, 1)
 
 st.write("Flugzeit")
-flighttime = st.slider("Bitte gib deine gewünschte Flugzeit von Zürich in Stunden ein: ",0, 5, 0)
+flighttime = st.selectbox("Bitte gib die maximal zulässige Flugzeit von Zürich ein: ", ["weniger als 2 Stunden", "2 bis 4 Stunden", "mehr als 4 Stunden"], index = None, placeholder = "Bitte wählen")
 
 st.write("Budget")
 budget = st.number_input("Bitte gib dein gewünschtes Budget für die Reise ein:", min_value = 0, step = 100)
@@ -30,7 +36,7 @@ trip_end = st.date_input("Bitte gib dein gewünschten Endzeitpunkt ein:")
 trip_duration = trip_end - trip_start
 
 trip = {
-    "Temperatur": temperature, "Sicherheit": safety, "Flugzeit": flighttime, "Budget": budget, "Reisestart": trip_start, "Reiseende": trip_end, "Reisedauer": trip_duration
+    "Art des Reiseziels": category, "Temperatur": temperature, "Sicherheit": safety, "Flugzeit": flighttime, "Budget": budget, "Reisestart": trip_start, "Reiseende": trip_end, "Reisedauer": trip_duration
 }
 
 st.subheader("Zusammenfassung")
@@ -38,7 +44,3 @@ st.write("Du hast folgende Inputfaktoren gewählt: ")
 
 for key, value in trip.items():
     st.markdown(f"**{key}:** {value}")
-
-st.title("Test, ob diese Kacke funktioniert")
-
-st.title("Test 2")
